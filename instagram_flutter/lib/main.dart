f50429b0b2fae31bec37f99977bc5cd70a6ab5a1
AbhilashTUofficial <abhilashstorm@gmail.com>
2021-12-23 13:36:21 +0530

    Firebase setup

--- instagram_flutter/lib/main.dart
+++ instagram_flutter/lib/main.dart
@@ -1,10 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDcSjTQ_4XyallsBEY5UoxQ_L3IGodJqRE",
+          authDomain: "instagram-clone-d4776.firebaseapp.com",
+          projectId: "instagram-clone-d4776",
+          storageBucket: "instagram-clone-d4776.appspot.com",
+          messagingSenderId: "776629533881",
+          appId: "1:776629533881:web:d2e0931da233fa84e06f68"
+      ),
+    );
+
+  }
+  else{
+    await Firebase.initializeApp();
+
+  }
   runApp(const MyApp());
 }
 
